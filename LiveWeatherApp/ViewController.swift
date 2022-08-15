4dc67c998c70d707eba3ab4551bf3b9f8bf4e8c2
minhye9731 <jenney62639731@gmail.com>
2022-08-15 16:55:37 +0900

    Initial Commit

---
+++ LiveWeatherApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LiveWeatherApp
+//
+//  Created by 강민혜 on 8/15/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
